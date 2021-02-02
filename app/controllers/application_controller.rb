48bf0f49634fdba4a1e9925375e8bcfeede48f1a
ana sanots <anasoliveiraaa@gmail.com>
2021-02-02 15:28:40 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["rentapool.today "] || "localhost:3000" }
+    { host: ENV["www.rentapool.today"] || "localhost:3000" }
   end
 end
